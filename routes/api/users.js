ea471e3d4914c6f48db5736a21aa40624f91a28d
Eduardo Urbina Dev <eduardoh.urbina.developer@gmail.com>
2019-10-06 03:51:49 -0500

    Added User schema

---
+++ routes/api/users.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const router = express.Router(); 
+const { check, validationResult } = require('express-validator/check');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
+// @route   POST api/users
+// @desc    Register user
+// @access  Public 
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter password with 6 or more characters').isLength({min:6})
+] ,async (req, res) => { 
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array()})
+    }
+
+    const { name, email, password } = req.body; 
+
+    try {
+        //This is MongoDB documentation
+        let user = await User.findOne({ email });
+
+        // See if user exists 
+        if(user) {
+            res.status(400).json({ errors: [ { msg: 'User already exists' }]});
+        }
+
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+        // Encrypt password
+        
+        // Return the jsonwebtoken 
+        
+    res.send('User registered');
+
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+}) 
+
+module.exports = router; 
