e9bae28e597a751dc0ae56137fe76ea2ab702ca3
Eduardo Urbina Dev <eduardoh.urbina.developer@gmail.com>
2019-10-10 21:56:53 -0500

    Added Profile Schema

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router(); 
-const { check, validationResult } = require('express-validator/check');
 const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken'); 
 const config = require('config')
+const { check, validationResult } = require('express-validator/check');
 // @route   POST api/users
 // @desc    Register user
 // @access  Public 
