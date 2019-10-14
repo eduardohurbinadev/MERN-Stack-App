23a509ac10ea0c3aa1908028185e4ac5b96945b5
Eduardo Urbina Dev <eduardoh.urbina.developer@gmail.com>
2019-10-13 20:35:30 -0500

    Added Github profile api oath authentication

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router(); 
+const request = require('request');
+const config = require('config');
 const auth = require('../../middleware/auth')
 const { check, validationResult } = require('express-validator/check');
 
@@ -280,5 +282,33 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from Github
+// @access  Public
+router.get('/github/:username', (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&
+            sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers: { 'user-agent': 'node.js' }
+        };
+
+        request(options, (error, response, body) => {
+            if(error) console.error(error);
+
+            if(response.statusCode != 200) {
+               return res.status(404).json({ msg: 'No Github profile found' });
+            }
+
+            res.json(JSON.parse(body));
+        });
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send("Server Error");
+    }
+})
+
 
 module.exports = router; 
