3a4e2122777d624cb332d6457254fbd7865d46d9
Eduardo Urbina Dev <eduardoh.urbina.developer@gmail.com>
2019-10-15 22:18:42 -0500

    Added delete and create post comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,82 @@
 const express = require('express');
-const router = express.Router(); 
+const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../../middleware/auth') 
+
+const Post = require('../../models/Post')
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
+
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private 
+
+router.post('/', [auth, [
+    check('text', 'Text is required').not().isEmpty()
+]],
+async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() }); 
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newPost = new Post ({
+            text: req.body.text,
+            name: user.name, 
+            user: req.user.id
+        });
+
+        const post = await newPost.save(); 
+
+        res.json(post);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
 
 // @route   GET api/posts
-// @desc    Test route
-// @access  Public 
-router.get('/', (req, res) => res.send('Posts route')); 
+// @desc    Get all posts
+// @access  Private 
+
+router.get('/', auth, async(req, res) => {
+    try {
+        const posts = await Post.find().sort( { date: -1 });
+        res.json(posts);
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+    })
+});
+
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private 
+
+router.delete('/:id', auth, async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({msg: 'Post'})
+        }
+
+        // Check user
+        if(post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized'});
+        }
+
+        await post.remove();
+
+        res.json({ msg: 'Post removed' });
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+    });
 
 module.exports = router; 
