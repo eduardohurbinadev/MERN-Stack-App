b3cbf37fb2ad7a16afede85567e7f2bbf852a32f
Eduardo Urbina Dev <eduardoh.urbina.developer@gmail.com>
2019-10-19 01:58:26 -0500

    Added the login and register pages

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { Fragment, useState } from "react";
+import { Link } from 'react-router-dom'
 
+//This is a hook, so instead of a class you put one.
 const Login = () => {
-    return (
-        <div>
-            Login
-        </div>
-    )
-}
+	const [formData, setFormData] = useState({
+		email: "",
+		password: ""
+	});
 
-export default Login
+	const { email, password } = formData;
+
+	const onChange = e =>
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = async e => {
+        e.preventDefault();
+			console.log("SUCCESS")
+    }
+	return (
+		<Fragment>
+			<h1 className='large text-primary'>Sign In</h1>
+			<p className='lead'>
+				<i className='fas fa-user'></i> Sign Into Your Account
+			</p>
+			<form className='form' onSubmit = { e => onSubmit(e) }>
+				<div className='form-group'>
+					<input
+						type='email'
+						placeholder='Email Address'
+						name='email'
+						value={email}
+						onChange={e => onChange(e)}
+					/>
+					<small className='form-text'>
+						This site uses Gravatar so if you want a profile image, use a
+						Gravatar email
+					</small>
+				</div>
+				<div className='form-group'>
+					<input
+						type='password'
+						placeholder='Password'
+						name='password'
+						value={password}
+						onChange={e => onChange(e)}
+						minLength='6'
+					/>
+				</div>
+				<input type='submit' className='btn btn-primary' value='Login' />
+			</form>
+			<p className='my-1'>
+				Don't have an account? <Link to='/register'>Sign Up</Link>
+			</p>
+		</Fragment>
+	);
+};
+
+export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,7 @@
 import React, { Fragment, useState } from "react";
+import { Link } from 'react-router-dom'
+//import axios from 'axios'; 
+
 //This is a hook, so instead of a class you put one.
 const Register = () => {
 	const [formData, setFormData] = useState({
@@ -13,12 +16,31 @@ const Register = () => {
 	const onChange = e =>
 		setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    const onSubmit = e => {
+    const onSubmit = async e => {
         e.preventDefault();
         if (password != password2) {
             console.log('Passwords do not match')
         } else {
-            console.log(formData);
+/*             const newUser = {
+				name, 
+				email,
+				password
+			} 
+			try {
+				const config = {
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				}
+
+				const body = JSON.stringify(newUser); 
+
+				const res = await axios.post('/api/users', body, config);
+				console.log(res.data); 
+			} catch (err) {
+				console.error(err.response.data)
+			} */
+			console.log("SUCCESS")
         }
     }
 	return (
@@ -74,7 +96,7 @@ const Register = () => {
 				<input type='submit' className='btn btn-primary' value='Register' />
 			</form>
 			<p className='my-1'>
-				Already have an account? <a href='login.html'>Sign In</a>
+				Already have an account? <Link to='/login'>Sign In</Link>
 			</p>
 		</Fragment>
 	);
